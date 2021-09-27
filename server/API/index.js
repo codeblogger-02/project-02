31d438a4bf33d5c950585a9d0142f3ecf101b0f2
codeblogger-02 <mahendranimsatkar02@gmail.com>
2021-09-27 11:25:15 +0530

    added API

---
+++ server/API/index.js
@@ -0,0 +1,51 @@
+import express, { Router } from "express";
+
+//Database model 
+import { ImageModel } from "../Database/image";
+
+
+//Routes
+
+/*
+route      /
+des         uploads given image to the Database
+params      none
+Access      public
+method      post 
+*/
+
+Router.post("/", async(req,res) => {
+    try {
+
+        const {newImage} = req.body;
+        await ImageModel.create(newImage);
+
+        return res.json({message: "Image has been added!!!"});
+        
+    } catch (error) {
+        return res.json({error: error.message});
+    }
+})
+
+
+/*
+route      /
+des         get an particular image based on catogery
+params      none
+Access      params
+method      get 
+*/
+
+
+Router.get("/c/:category" , async(req,res) => {
+    try {
+        const getSpecificImage = await ImageModel.findOne({category: req.params.category});
+        if (!getSpecificImage){
+            return res.json({
+                error: `No image found for category of ${req.params.category}`,
+            });
+        }
+    } catch (error) {
+        return res.json({error: error.message});
+    }
+})
